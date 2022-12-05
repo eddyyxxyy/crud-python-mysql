cb8738cc3776e80316df817f75a9d44fbb8489a6
Edson Pimenta <edson.tibo@gmail.com>
2022-12-05 13:49:52 -0300

    Refactor: adds conection status animation

--- loja_python_mysql/main.py
+++ loja_python_mysql/main.py
@@ -1,5 +1,9 @@
 import utils
 
 if __name__ == '__main__':
+    with utils.cons.status(
+        '[b]Conectando[/b] ao banco de dados...', spinner='noise'
+    ):
+        utils.sleep(2)
     while True:
         utils.menu()
