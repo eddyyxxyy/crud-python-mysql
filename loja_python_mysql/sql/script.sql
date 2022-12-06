882416c4743368e94c68dbef974e4f51750d9c14
Edson Pimenta <edson.tibo@gmail.com>
2022-12-06 11:23:33 -0300

    Feat: adds inserts to test the app

--- loja_python_mysql/sql/script.sql
+++ loja_python_mysql/sql/script.sql
@@ -8,3 +8,9 @@ CREATE TABLE produtos (
     preco DECIMAL(8,2) NOT NULL,
     estoque INT NOT NULL
 );
+
+INSERT INTO produtos (nome, preco, estoque) VALUES ('Playstation 4', '1675.89', '25');
+INSERT INTO produtos (nome, preco, estoque) VALUES ('Xbox One', '1500.00', '30');
+INSERT INTO produtos (nome, preco, estoque) VALUES ('Nintendo Switch', '2545.99', '45');
+INSERT INTO produtos (nome, preco, estoque) VALUES ('Notebook Nitro 5', '4599.99', '5');
+INSERT INTO produtos (nome, preco, estoque) VALUES ('NoBreak', '459.99', '12');
